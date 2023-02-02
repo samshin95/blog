b844adb16407e14b7a9e0e6480515745a247eac5
samshin95 <samshin95@gmail.com>
2023-02-02 10:20:18 +0800

    footer

--- src/components/Footers.js
+++ src/components/Footers.js
@@ -10,7 +10,10 @@ export function Footers() {
           <span>{t("Get connected")}</span>
         </div>
         <div className="flex justify-center">
-          <a href="#!" className="mr-6 text-gray-600">
+          <a
+            href="https://www.facebook.com/profile.php?id=100001715688954"
+            className="mr-6 text-gray-600"
+          >
             <svg
               aria-hidden="true"
               focusable="false"
@@ -78,7 +81,10 @@ export function Footers() {
               ></path>
             </svg>
           </a> */}
-          <a href="#!" className="mr-6 text-gray-600">
+          <a
+            href="https://www.linkedin.com/in/sam-shin-a9b647246/"
+            className="mr-6 text-gray-600"
+          >
             <svg
               aria-hidden="true"
               focusable="false"
@@ -95,7 +101,7 @@ export function Footers() {
               ></path>
             </svg>
           </a>
-          <a href="#!" className="text-gray-600">
+          <a href="https://github.com/samshin95/blog" className="text-gray-600">
             <svg
               aria-hidden="true"
               focusable="false"
@@ -143,12 +149,9 @@ export function Footers() {
                   d="M488.6 250.2L392 214V105.5c0-15-9.3-28.4-23.4-33.7l-100-37.5c-8.1-3.1-17.1-3.1-25.3 0l-100 37.5c-14.1 5.3-23.4 18.7-23.4 33.7V214l-96.6 36.2C9.3 255.5 0 268.9 0 283.9V394c0 13.6 7.7 26.1 19.9 32.2l100 50c10.1 5.1 22.1 5.1 32.2 0l103.9-52 103.9 52c10.1 5.1 22.1 5.1 32.2 0l100-50c12.2-6.1 19.9-18.6 19.9-32.2V283.9c0-15-9.3-28.4-23.4-33.7zM358 214.8l-85 31.9v-68.2l85-37v73.3zM154 104.1l102-38.2 102 38.2v.6l-102 41.4-102-41.4v-.6zm84 291.1l-85 42.5v-79.1l85-38.8v75.4zm0-112l-102 41.4-102-41.4v-.6l102-38.2 102 38.2v.6zm240 112l-85 42.5v-79.1l85-38.8v75.4zm0-112l-102 41.4-102-41.4v-.6l102-38.2 102 38.2v.6z"
                 ></path>
               </svg>
-              Tailwind ELEMENTS
+              ELEMENTS
             </h6>
-            <p>
-              Here you can use rows and columns to organize your footer content.
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            </p>
+            <p>Put some element here.</p>
           </div>
           <div className="">
             <h6 className="uppercase font-semibold mb-4 flex justify-center md:justify-start">
@@ -256,7 +259,7 @@ export function Footers() {
                   d="M493.4 24.6l-104-24c-11.3-2.6-22.9 3.3-27.5 13.9l-48 112c-4.2 9.8-1.4 21.3 6.9 28l60.6 49.6c-36 76.7-98.9 140.5-177.2 177.2l-49.6-60.6c-6.8-8.3-18.2-11.1-28-6.9l-112 48C3.9 366.5-2 378.1.6 389.4l24 104C27.1 504.2 36.7 512 48 512c256.1 0 464-207.5 464-464 0-11.2-7.7-20.9-18.6-23.4z"
                 ></path>
               </svg>
-              + 04 2326 0832
+              + 09 2225 6556
             </p>
             <p className="flex items-center justify-center md:justify-start">
               <svg
