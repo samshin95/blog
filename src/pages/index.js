6914b7c7173be8aa5820968f1452540d06b477b1
samshin95 <samshin95@gmail.com>
2023-02-13 21:50:25 +0800

    change Link

--- src/pages/index.js
+++ src/pages/index.js
@@ -36,7 +36,7 @@ export default function Home({ allPostsData }) {
                   />
                 </Link>
                 <div class="p-5">
-                  <Link href="#">
+                  <Link href={`/posts/${id}`}>
                     <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
                       {title}
                     </h5>
@@ -45,7 +45,7 @@ export default function Home({ allPostsData }) {
                     <Dateview dateISO={date} />
                   </p>
                   <Link
-                    href="#"
+                    href={`/posts/${id}`}
                     class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                   >
                     Read more
